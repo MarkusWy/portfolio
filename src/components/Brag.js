b791a92d034e227d1a0c42d625fc2207f36f0d37
MarkusWy <markus.wyrin@gmail.com>
2023-10-08 22:12:16 +0200

    Added Brag component
    Signed-off-by: MarkusWy <markus.wyrin@gmail.com>

---
+++ src/components/Brag.js
@@ -0,0 +1,125 @@
+import * as React from "react"
+import styled from "@emotion/styled";
+import { Link } from "gatsby"
+
+const BragTitle = styled.div`
+  min-height: 100px;
+  font-size: 64px;
+  margin-bottom: 16px;
+`;
+
+const BragWrapper = styled.div`
+  min-height: 300px;
+  background-color: ${props => props.background || 'white'};
+  border-radius: 20px;
+  width: 100%;
+
+  @media (min-width: 768px) {
+    &:nth-child(3n+1) {
+      background: #f1f9ff;
+    }
+  }
+
+  @media (max-width: 768px) {
+    // select every other element
+    &:not(:nth-child(2n)) {
+      background: #f1f9ff;
+    }
+  }
+`;
+
+const BragInnerWrapper = styled.div`
+  padding: 32px;
+`;
+
+const BragSubtitle = styled.div`
+  min-height: 100px;
+  font-size: 25px;
+  margin-top: 16px;
+`;
+
+const LinkButton = styled(Link)`
+  background-color: #0081ff;
+  color: white;
+  padding: 15px 32px;
+  display: block;
+  width: fit-content;
+  border-radius: 10px;
+
+  &::hover {
+    background-color: white;
+    color: #0081ff;
+  }
+`;
+
+const LinkButtonWrapper = styled.div`
+  display: flex;
+`;
+
+const LinkDescription = styled.div`
+  margin: auto 16px;
+`;
+
+const BragGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 0;
+
+  @media (max-width: 768px) {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+  }
+`;
+
+const Brag = () => {
+  return (
+    <BragGrid>
+        <BragWrapper>
+            <BragInnerWrapper>
+            <BragTitle>🚀Optimized</BragTitle>
+            <LinkButtonWrapper>
+                <LinkButton to="https://pagespeed.web.dev/analysis?url=https%3A%2F%2Fmarkus.wyrin.se%2F">PageSpeed Insights</LinkButton>
+                <LinkDescription>Feel free to audit this site!</LinkDescription>
+            </LinkButtonWrapper>
+            <BragSubtitle>I pride myself in making well optimized, fast applications.</BragSubtitle>
+            </BragInnerWrapper>
+        </BragWrapper>
+
+        <BragWrapper>
+            <BragInnerWrapper>
+            <BragTitle>📱Mobile first</BragTitle>
+            <LinkButtonWrapper>
+                <LinkButton to="https://search.google.com/test/mobile-friendly?url=https%3A%2F%2Fmarkus.wyrin.se%2F">Mobile Friendly test</LinkButton>
+                <LinkDescription>See for yourself!</LinkDescription>
+            </LinkButtonWrapper>
+            <BragSubtitle>All my webapps are optimized for different devices.</BragSubtitle>
+            </BragInnerWrapper>
+        </BragWrapper>
+
+        <BragWrapper>
+            <BragInnerWrapper>
+            <BragTitle>🧼Clean</BragTitle>
+            <LinkButtonWrapper>
+                <LinkButton to="https://pagespeed.web.dev/analysis?url=https%3A%2F%2Fmarkus.wyrin.se%2F">GitHub</LinkButton>
+                <LinkDescription>See the source code for this site!</LinkDescription>
+            </LinkButtonWrapper>
+            <BragSubtitle>I write clean code without spaghetti 🍝</BragSubtitle>
+            </BragInnerWrapper>
+        </BragWrapper>
+
+        <BragWrapper>
+            <BragInnerWrapper>
+            <BragTitle>👨‍💻Contact me</BragTitle>
+            <LinkButtonWrapper>
+                <LinkButton to="#contact">Get in touch</LinkButton>
+                <LinkDescription>I look forward to hearing from you!</LinkDescription>
+            </LinkButtonWrapper>
+            <BragSubtitle>Do you want help with your business?<br/>get in touch!</BragSubtitle>
+            </BragInnerWrapper>
+        </BragWrapper>
+    </BragGrid>
+  )
+}
+
+export default Brag;
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,17 +5,37 @@ import Layout from "../components/layout";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Resume from "../components/Resume";
+import Brag from "../components/Brag";
 import { Link } from "gatsby"
 
 const BragTitle = styled.div`
   min-height: 100px;
-  font-size: 100px;
+  font-size: 64px;
   margin-bottom: 16px;
 `;
 
 const BragWrapper = styled.div`
   min-height: 300px;
-  padding:32px;
+  background-color: ${props => props.background || 'white'};
+  border-radius: 20px;
+  width: 100%;
+
+  @media (min-width: 768px) {
+    &:nth-child(3n+1) {
+      background: #f1f9ff;
+    }
+  }
+
+  @media (max-width: 768px) {
+    // select every other element
+    &:not(:nth-child(2n)) {
+      background: #f1f9ff;
+    }
+  }
+`;
+
+const BragInnerWrapper = styled.div`
+  padding: 32px;
 `;
 
 const BragSubtitle = styled.div`
@@ -49,7 +69,13 @@ const LinkDescription = styled.div`
 const BragGrid = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
-  grid-gap: 16px;
+  grid-gap: 0;
+
+  @media (max-width: 768px) {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+  }
 `;
 
 const Home = () => {
@@ -58,43 +84,7 @@ const Home = () => {
       <title>Markus Wyrin</title>
       <Header/>
       <Hero/>
-      <BragGrid>
-        <BragWrapper>
-          <BragTitle>🚀Optimized</BragTitle>
-          <LinkButtonWrapper>
-            <LinkButton to="https://pagespeed.web.dev/analysis?url=https%3A%2F%2Fmarkus.wyrin.se%2F">PageSpeed Insights</LinkButton>
-            <LinkDescription>Feel free to audit this site!</LinkDescription>
-          </LinkButtonWrapper>
-          <BragSubtitle>I pride myself in making well optimized, fast applications.</BragSubtitle>
-        </BragWrapper>
-
-        <BragWrapper>
-          <BragTitle>🚀Optimized</BragTitle>
-          <LinkButtonWrapper>
-            <LinkButton to="https://pagespeed.web.dev/analysis?url=https%3A%2F%2Fmarkus.wyrin.se%2F">PageSpeed Insights</LinkButton>
-            <LinkDescription>Feel free to audit this site!</LinkDescription>
-          </LinkButtonWrapper>
-          <BragSubtitle>I pride myself in making well optimized, fast applications.</BragSubtitle>
-        </BragWrapper>
-
-        <BragWrapper>
-          <BragTitle>🚀Optimized</BragTitle>
-          <LinkButtonWrapper>
-            <LinkButton to="https://pagespeed.web.dev/analysis?url=https%3A%2F%2Fmarkus.wyrin.se%2F">PageSpeed Insights</LinkButton>
-            <LinkDescription>Feel free to audit this site!</LinkDescription>
-          </LinkButtonWrapper>
-          <BragSubtitle>I pride myself in making well optimized, fast applications.</BragSubtitle>
-        </BragWrapper>
-
-        <BragWrapper>
-          <BragTitle>🚀Optimized</BragTitle>
-          <LinkButtonWrapper>
-            <LinkButton to="https://pagespeed.web.dev/analysis?url=https%3A%2F%2Fmarkus.wyrin.se%2F">PageSpeed Insights</LinkButton>
-            <LinkDescription>Feel free to audit this site!</LinkDescription>
-          </LinkButtonWrapper>
-          <BragSubtitle>I pride myself in making well optimized, fast applications.</BragSubtitle>
-        </BragWrapper>
-      </BragGrid>
+      <Brag/>
       <Resume/>
     </Layout>
   )

c59d457aa6660578af0eb755b987a72f4451e182
MarkusWy <markus.wyrin@gmail.com>
2023-10-08 22:12:16 +0200

    added needed basic packages
    Signed-off-by: MarkusWy <markus.wyrin@gmail.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,21 @@
 import * as React from "react"
+import { StaticImage } from "gatsby-plugin-image"
+import styled from "@emotion/styled";
+
+const Wrapper = styled.div`
+  width: 100%;
+`;
+
+const Title = styled.h1`
+  color: black;
+`;
 
 export default function Home() {
-  return <div>Hello world!</div>
+
+  return (
+    <Wrapper>
+      <StaticImage src="https://placekitten.com/1000/600" alt="A kitten" />
+      <Title>Markus Wyrin</Title>
+    </Wrapper>
+  )
 }

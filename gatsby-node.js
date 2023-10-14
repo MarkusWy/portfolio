9a1656abd707451d1430cf3507fb3ddb1b42e9c2
MarkusWy <markus.wyrin@gmail.com>
2023-10-14 12:12:26 +0200

    Added layout to blogposttemplate and cleaned up the gatsby-node data transformation a bit

--- gatsby-node.js
+++ gatsby-node.js
@@ -40,24 +40,32 @@ exports.createPages = async ({ graphql, actions }) => {
   `)
   result.data.allPrismicBlogPost.edges.forEach(edge => {
 
-    let postContent = edge.node.data.blog_post_content;
+    const postContent = edge.node.data.blog_post_content;
 
-    const newPostContent = postContent.map(post => {
+    // This is used to create responsive images from prismic wysiwyg data for our blog posts.
+    const transformedPostContent = postContsent.map(post => {
+      // This gets the content inside quotes ""
       const srcRegex = /"(.*?)"/g;
+      // This gets the whole image tag from opening to closing />
       const imgRegex = /<img[^>]+>/g;
+      // Gets an array of image elements from the wysiwyg html string
       const imgElements = [...post.prismic_wysiwyg.html.match(imgRegex) || []];
     
+      // We loop through and create a srcset for each image element
       imgElements.forEach(imgElement => {
+        // This gets the image tag string
         const imgPath = imgElement.match(srcRegex)[0];
 
+        // we prep our sources
         const srcExtraSmall = `<source media="(min-width:300px)" srcset=${imgPath.replace('compress,format', 'compress,format&width=300&ar=1:1&fit=crop')}/>`
         const srcSmall = `<source media="(min-width:650px)" srcset=${imgPath.replace('compress,format', 'compress,format&width=650&ar=1:1&fit=crop')}/>`
         const srcMedium = `<source media="(min-width:1000px)" srcset=${imgPath.replace('compress,format', 'compress,format&width=1000&ar=1:1&fit=crop')}/>`
         const srcLarge = `<source media="(min-width:1500px)" srcset=${imgPath.replace('compress,format', 'compress,format&width=1500&ar=1:1&fit=crop')}/>`
 
+        // We inject the sources into the html string
         post.prismic_wysiwyg.html = post.prismic_wysiwyg.html.replace(imgElement, `<picture>${srcLarge}${srcMedium}${srcSmall}${srcExtraSmall}${imgElement}</picture>`);
       });
-    
+
       return post;
     });
 
@@ -66,7 +74,7 @@ exports.createPages = async ({ graphql, actions }) => {
       component: blogPostTemplate,
       context: {
         title: edge.node.data.post_title.text,
-        postContent: newPostContent
+        postContent: transformedPostContent
       },
     })
   })
--- src/templates/BlogPostTemplate.js
+++ src/templates/BlogPostTemplate.js
@@ -1,23 +1,48 @@
 import React from "react";
 import Wysiwyg from "../components/Wysiwyg";
+import Layout from "../components/layout";
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import Footer from "../components/Footer";
+import Seo from "../components/seo";
+import styled from "@emotion/styled";
+
+
+const ComponentsWrapper = styled.div`
+  margin: auto;
+  max-width: 1200px;
+  padding: 0 32px;
+  align-items: center;
+`;
 
 const BlogPostTemplate = ({location, pageContext, pageResources, params, path, serverData, uri, ...rest}) => {
-    console.log(pageContext);
-
     const postContent = pageContext.postContent;
 
     console.log(postContent);
 
     return (
       <>
-        {postContent.map ((post, index) => (
-            <div key={index}>
-                <Wysiwyg html={post.prismic_wysiwyg.html}/>
-            </div>
-        ))}
-        <h1>Blog Post</h1>
+        <Layout>
+          <Header/>
+          <Hero subtitle="This is what I have accomplished so far"/>
+          <ComponentsWrapper>
+          <h1>Blog Post</h1>
+            {postContent.map ((post, index) => (
+              <div key={index}>
+                  <Wysiwyg html={post.prismic_wysiwyg.html}/>
+              </div>
+            ))}
+          </ComponentsWrapper>
+          <Footer/>
+        </Layout>
       </>
     )
+}
+
+export function Head() {
+  return (
+    <Seo/>
+  )
 }
 
 export default BlogPostTemplate;
